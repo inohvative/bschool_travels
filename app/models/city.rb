b6655586bd8a38f4a5e4943e63cbd069ac1d24d5
Raghu Betina <raghu@firstdraft.com>
2019-12-24 02:40:28 +0000

    Direct association between cities and travel_personas

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,10 @@
 class City < ApplicationRecord
   # Direct associations
 
+  has_many   :travel_preferences,
+             :class_name => "TravelPersona",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
