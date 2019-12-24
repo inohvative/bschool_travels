506e503934d182a67cd7c4a91f9bc52cf60019c0
Raghu Betina <raghu@firstdraft.com>
2019-12-24 02:41:04 +0000

    Whitelist columns in EnergyLevel for ActiveAdmin

--- app/admin/energy_levels.rb
+++ app/admin/energy_levels.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register EnergyLevel do
+
+ permit_params :party_level
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
